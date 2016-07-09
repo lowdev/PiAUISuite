df01fc5973f06fe16fb47508352561647670a6c8
Laury Lafage <lowrey97x@hotmail.fr>
2016-07-09 23:02:36 +0200

    Fix the indentation for InstallAUISuite.sh.

--- Install/InstallAUISuite.sh
+++ Install/InstallAUISuite.sh
@@ -6,16 +6,16 @@ function dependencies() {
     #Ask to install dependencies
     install_method=`pgrep apt-get`
     if [ -z $install_method ] ; then
-    echo "Install dependencies? y/n"
-    echo "These are necessary for any of the options, so you should probably press y unless you absolutely know you have them already"
-    read option
-    if [ $option == "y" ] || [ $option == "Y" ] ; then
-        if [ "$ARCH" == "armv6l" ] ; then
-            sudo apt-get install locate curl libboost-dev libboost-regex-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-openssl-dev flac sox
-        else
-            sudo apt-get install locate curl libboost-dev libboost-regex-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-openssl-dev flac sox
-        fi
-    fi
+		echo "Install dependencies? y/n"
+		echo "These are necessary for any of the options, so you should probably press y unless you absolutely know you have them already"
+		read option
+		if [ $option == "y" ] || [ $option == "Y" ] ; then
+			if [ "$ARCH" == "armv6l" ] ; then
+				sudo apt-get install locate curl libboost-dev libboost-regex-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-openssl-dev flac sox
+			else
+				sudo apt-get install locate curl libboost-dev libboost-regex-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-openssl-dev flac sox
+			fi
+		fi
     fi
 }
 
@@ -25,13 +25,13 @@ function playvideo_install() {
     echo "This script indexes your movies and tv shows in order to quickly find, randomize, and/or play them. I find it extremely useful"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
-    echo "Installing playvideo"
-    echo "Enter movie/video root folder location, ex: /media/External/movies"
-    read media_path
-    `../PlayVideoScripts/playvideo -set "$media_path"`
-    sudo cp ../PlayVideoScripts/playvideo /usr/bin/playvideo
-    sudo cp ../PlayVideoScripts/playvideo.8.gz /usr/share/man/man8/
-    echo "Done installing playvideo!"
+		echo "Installing playvideo"
+		echo "Enter movie/video root folder location, ex: /media/External/movies"
+		read media_path
+		`../PlayVideoScripts/playvideo -set "$media_path"`
+		sudo cp ../PlayVideoScripts/playvideo /usr/bin/playvideo
+		sudo cp ../PlayVideoScripts/playvideo.8.gz /usr/share/man/man8/
+		echo "Done installing playvideo!"
     else
         echo "Skipping playvideo install"
     fi
@@ -45,28 +45,28 @@ function download() {
     echo "This script automates the download of torrents. Ex. 'download wheezy' finds and downloads the newest debian wheezy image"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
-    echo "Installing downloader script"
-    echo "Enter host: ex, localhost (this is probably what it is)"
-    read host
-    echo "Enter Port: default is 9091 I believe"
-    read port
-    echo "Enter username, press enter if none"
-    read user
-    echo "Enter password, press enter if none"
-    read passwd
-    #print commands to $USER_HOME/.down.info
-    if [ -e "$USER_HOME/.down.info" ] ; then
-        sudo rm -f "$USER_HOME/.down.info"
-    fi
-    echo "$host"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-    echo "$port"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-    echo "$user"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-    echo "$passwd" | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-    tmp="../DownloadController/"
-    tmp+="$DIR"
-    tmp+="download"
-    sudo cp "$tmp" /usr/bin/download
-    echo "Done installing download!"
+		echo "Installing downloader script"
+		echo "Enter host: ex, localhost (this is probably what it is)"
+		read host
+		echo "Enter Port: default is 9091 I believe"
+		read port
+		echo "Enter username, press enter if none"
+		read user
+		echo "Enter password, press enter if none"
+		read passwd
+		#print commands to $USER_HOME/.down.info
+		if [ -e "$USER_HOME/.down.info" ] ; then
+			sudo rm -f "$USER_HOME/.down.info"
+		fi
+		echo "$host"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+		echo "$port"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+		echo "$user"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+		echo "$passwd" | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+		tmp="../DownloadController/"
+		tmp+="$DIR"
+		tmp+="download"
+		sudo cp "$tmp" /usr/bin/download
+		echo "Done installing download!"
     else
         echo "Skipping downloader install"
     fi
@@ -110,46 +110,46 @@ function gtextcommand {
     echo "This installs gtextcommand. This uses google voice to check for system commands from your number with a passcode."
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
-    echo "Installing Text Command Script"
-    echo "Enter google voice username: "
-    read user
-    echo "Enter google voice password: "
-    read passwd
-    echo "Enter command keyword, ex: Cmd (this will proceed every vaild command)"
-    read key
-    echo "Enter valid number to accept commands from (make sure to put the country code but not the +) ex: 15553334444"
-    read number
-    #print commands to $USER_HOME/.gtext
-    if [ -e "$USER_HOME/.gtext" ] ; then
-        sudo rm -f "$USER_HOME/.gtext"
-    fi
-    echo "$user"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-    echo "$passwd"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-    echo "$key"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-    echo "$number" | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-    tmp="../TextCommand/"
-    tmp+="$DIR"
-    tmp+="gtextcommand"
-    sudo cp "$tmp" /usr/bin/gtextcommand
-    #Add to cron.d
-    echo "Done installing, setting up cron script ..."
-    if [ -e "/etc/cron.d/gtextcommand" ] ; then
-        sudo rm -f "/etc/cron.d/gtextcommand"
-    fi
-    cronjob="#!/bin/sh
-    #
-    # cron script to check google voice.
-    #
-    # Written by Steven Hickson <help@stevenhickson.com> for the gtextcommand script.
-    #
-    DISPLAY=:0
+		echo "Installing Text Command Script"
+		echo "Enter google voice username: "
+		read user
+		echo "Enter google voice password: "
+		read passwd
+		echo "Enter command keyword, ex: Cmd (this will proceed every vaild command)"
+		read key
+		echo "Enter valid number to accept commands from (make sure to put the country code but not the +) ex: 15553334444"
+		read number
+		#print commands to $USER_HOME/.gtext
+		if [ -e "$USER_HOME/.gtext" ] ; then
+			sudo rm -f "$USER_HOME/.gtext"
+		fi
+		echo "$user"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+		echo "$passwd"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+		echo "$key"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+		echo "$number" | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+		tmp="../TextCommand/"
+		tmp+="$DIR"
+		tmp+="gtextcommand"
+		sudo cp "$tmp" /usr/bin/gtextcommand
+		#Add to cron.d
+		echo "Done installing, setting up cron script ..."
+		if [ -e "/etc/cron.d/gtextcommand" ] ; then
+			sudo rm -f "/etc/cron.d/gtextcommand"
+		fi
+		cronjob="#!/bin/sh
+		#
+		# cron script to check google voice.
+		#
+		# Written by Steven Hickson <help@stevenhickson.com> for the gtextcommand script.
+		#
+		DISPLAY=:0
 
-    * * * * * $USER gtextcommand
+		* * * * * $USER gtextcommand
 
-    "
-    echo "$cronjob" | sudo tee -a /etc/cron.d/gtextcommand >/dev/null
-    sudo sh -c 'chmod +x /etc/cron.d/gtextcommand'
-    echo "Done installing gtextcommand!"
+		"
+		echo "$cronjob" | sudo tee -a /etc/cron.d/gtextcommand >/dev/null
+		sudo sh -c 'chmod +x /etc/cron.d/gtextcommand'
+		echo "Done installing gtextcommand!"
     else
         echo "Skipping gtextcommand install"
     fi
